4d670e96b133e26ce1b043d01b25145d08c8ac10
quan281999 <quan281999@gmail.com>
2023-02-12 15:41:00 +0700

    remove redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import { type AppType } from "next/app";
-import React, { useState, createContext } from "react";
-import { Provider } from "react-redux";
+import React, { createContext } from "react";
 
 import { api } from "../utils/api";
 import "../styles/globals.css";
-import { wrapper } from "../store";
 import Layout from "../components/Layout";
 import CustomThemeProvider from "../components/shared/CustomThemeProvider";
 
@@ -16,22 +14,17 @@ export const AuthContext = createContext<TAuthContext>({
   userId: "63701cc1f03239b7f700000e",
 });
 
-const MyApp: AppType = ({ Component, ...rest }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { store, props } = wrapper.useWrappedStore(rest);
-
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <CustomThemeProvider>
-        {/* MOCK DEFAULT USER, AUTHENTICATION IS NOT IMPLEMENTED IN THIS SMALL APP */}
-        <AuthContext.Provider value={{ userId: "63701cc1f03239b7f700000e" }}>
-          <Layout>
-            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
-            <Component {...props.pageProps} />
-          </Layout>
-        </AuthContext.Provider>
-      </CustomThemeProvider>
-    </Provider>
+    <CustomThemeProvider>
+      {/* MOCK DEFAULT USER, AUTHENTICATION IS NOT IMPLEMENTED IN THIS SMALL APP */}
+      <AuthContext.Provider value={{ userId: "63701cc1f03239b7f700000e" }}>
+        <Layout>
+          {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContext.Provider>
+    </CustomThemeProvider>
   );
 };
 
